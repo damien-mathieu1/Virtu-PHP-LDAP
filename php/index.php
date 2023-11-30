c182114a5f90d32981a354f4df765229c2b79dc5
damien MATHIEU <damien.trot34@gmail.com>
2023-11-30 17:26:05 +0100

    working connection with docker ldap and configuration to iut ldap with CONFIG env in docker compose

---
+++ php/index.php
@@ -0,0 +1,10 @@
+
+<form action="login.php" method="POST">
+  <label for="username">Username:</label>
+  <input type="text" id="username" name="username" required><br><br>
+  
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password" required><br><br>
+  
+  <input type="submit" value="Login">
+</form>
---
+++ php/login.php
@@ -0,0 +1,46 @@
+
+<?php
+// FILEPATH: /home/damien/iut/S5/virtu/Virtu-PHP-LDAP/login.php
+
+// LDAP server details
+$config = getenv('CONFIG');
+if($config == 'docker'){
+    $ldapServer = 'ldap';
+    $ldapBaseDn = "dc=example,dc=com";
+    $ldapPort = 389;
+}
+elseif($config == 'iut'){
+    $ldapServer = "10.10.1.30";
+    $ldapBaseDn = "dc=info,dc=iutmontp,dc=univ-montp2,dc=fr";
+    $ldap_port = 389;
+}
+else{
+    die('CONFIG environment variable not set (docker or local)');
+}
+$ldapConn = false;
+
+// User credentials
+$username = $_POST['username']; 
+$password = $_POST['password'];
+
+// Connect to the LDAP server
+$ldapConn = ldap_connect($ldapServer, $ldapPort);
+if (!$ldapConn) {
+    die('Failed to connect to LDAP server');
+}
+
+// Set LDAP options
+ldap_set_option($ldapConn, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+// Bind to the LDAP server with user credentials
+$ldapBind = ldap_bind($ldapConn, $username, $password);
+if (!$ldapBind) {
+    die('LDAP authentication failed');
+}
+
+// LDAP authentication successful
+echo 'LDAP authentication successful';
+
+// Close the LDAP connection
+ldap_unbind($ldapConn);
+?>
